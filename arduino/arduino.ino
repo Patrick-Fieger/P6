49c4fc5de33c8721d6bd5ea715d97bae5239d8e7
Patrick Fieger <patrickfieger90@gmail.com>
2016-06-13 19:06:24 +0200

    update

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,40 +1,41 @@
 #include <Bridge.h>
+#include <Servo.h>
 #include <YunServer.h>
 #include <YunClient.h>
 
-const byte internalLED = 13;
-
 YunServer server;
+Servo myservo;
+int pos = 0; 
 
 void setup() {
-  pinMode(internalLED, OUTPUT);
-  digitalWrite(internalLED, HIGH);     // Turn the led on to advise about bridge begin in progress
   Bridge.begin();          // Let's activate the Yun Bridge...
-  ledBlink(150);           // ...bridge activation done (user sees a blink)
+  myservo.attach(9);
   // server.listenOnLocalhost();       // Listen for incoming connection only from localhost (no one from the external network could connect)
   server.begin();          // Let's acrivate the Yun server...
-  ledBlink(150);           // ...server activation done (user sees a blink)
+
 }
 
 void loop() {
+  Serial.println(analogRead(5));
+  
   YunClient client = server.accept();  // Get clients coming from server
   if (client)                          // Is there a new client?
   {
 
     String command = client.readString();
     command.trim();        //kill whitespace
+
+    if(command == "1"){
+        myservo.write(90);              // tell servo to go to position in variable 'pos'
+    }else if(command == "3"){
+          myservo.write(180);              // tell servo to go to position in variable 'pos'
+    }
+    
     Serial.println(command);
     client.print(command);
-    client.print(77);                  // Send a "77" to client (e.g. browser)
     client.stop();                     // Close connection and free resources.
-    ledBlink(500);
   }
   delay(50);
 }
 
-void ledBlink(int delayTime) {
-  digitalWrite(internalLED, HIGH); // Turn (or keep) the LED on
-  delay(delayTime);
-  digitalWrite(internalLED, LOW);  // Turn (or keep) the LED off
-  delay(delayTime);
-}
+
