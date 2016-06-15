cf298353e43f35e44d69f0da45dc463e1a877120
Patrick Fieger <patrickfieger90@gmail.com>
2016-06-15 18:30:23 +0200

    new arduino file

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,41 +1,69 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 #include <Bridge.h>
-#include <Servo.h>
 #include <YunServer.h>
 #include <YunClient.h>
 
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  650 // this is the 'maximum' pulse length count (out of 4096)
+
+int maxItemsInArray = 3;
+int pos[3][2][2]={
+ {{0,0},{1,0}},
+ {{0,90},{1,90}},
+ {{0,180},{1,180}},
+};
+
+int lastpos[2] = {0,0};
+
 YunServer server;
-Servo myservo;
-int pos = 0; 
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void setup() {
-  Bridge.begin();          // Let's activate the Yun Bridge...
-  myservo.attach(9);
-  // server.listenOnLocalhost();       // Listen for incoming connection only from localhost (no one from the external network could connect)
+  Bridge.begin();           // Let's activate the Yun Bridge...
+  pwm.begin();
+  pwm.setPWMFreq(60);       // Analog servos run at ~60 Hz updates
   server.begin();          // Let's acrivate the Yun server...
-
 }
 
 void loop() {
-  Serial.println(analogRead(5));
-  
-  YunClient client = server.accept();  // Get clients coming from server
-  if (client)                          // Is there a new client?
-  {
+  YunClient client = server.accept();
 
+  if (client){
     String command = client.readString();
-    command.trim();        //kill whitespace
-
-    if(command == "1"){
-        myservo.write(90);              // tell servo to go to position in variable 'pos'
-    }else if(command == "3"){
-          myservo.write(180);              // tell servo to go to position in variable 'pos'
+    command.trim();
+    if(command == "0"){
+      edit();
+    }else{
+      setPositions(command);
     }
-    
-    Serial.println(command);
-    client.print(command);
-    client.stop();                     // Close connection and free resources.
+
+    client.stop();
   }
-  delay(50);
 }
 
+void setPositions(String number){
+  int num = number.toInt();
 
+  for(int i=0; i < maxItemsInArray; i++){
+      int deg = map(pos[num][i][1],0,180,SERVOMIN,SERVOMAX);
+
+      if(lastpos[i] > deg){
+        for (int pulselen = lastpos[i]; lastpos[i] > deg; pulselen--) {
+            pwm.setPWM(pos[num][i][0], 0, pulselen);
+            delay(10);
+        }
+      }else{
+        for (int pulselen = lastpos[i]; lastpos[i] < deg; pulselen++) {
+            pwm.setPWM(pos[num][i][0], 0, pulselen);
+            delay(10);
+        }
+      }
+
+      lastpos[i] = pos[num][i][1];
+  }
+}
+
+void edit(){
+
+}
