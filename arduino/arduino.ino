5f9483311b205ac7ee9d8b904042ec314821aade
Tim J. Peters <tim_peters@gmx.de>
2016-07-02 17:51:19 +0200

    added resetPositions() and redesigned some variables

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,31 +3,29 @@
 
 
 // situations > furnitures > levels > pin, angle
-int target_pos[3][2][3][2]={
+int target_pos[3][3][3][2]={
   { // Stuhl und tisch
-   {{3,1},{2,180},{1,1}},
-   {{1,90},{2,90}}
+   {{1,180},{2,100},{3,90}}, // S
+   {{4,180},{5,150},{6,170}}, // T
+   {{8,0},{9,3}}, // STB
   },
   { // liege
-    {{1,120},{2,10}}
+   {{1,10},{2,90},{3,90}}, // S
+   {{4,90},{5,90},{6,170}}, // T
+   {{8,90},{9,100}}, // STB
   },
  { // Gäste
-    {{1,180},{2,180}}
- }
+   {{1,10},{2,90},{3,90}}, // S
+   {{4,90},{5,90},{6,170}}, // T
+   {{8,90},{9,100}}, // STB
+  },
 };
 
-int current_pos[3][2][3][2],
- initial_pos[3][2][3][2]={
-  { // Stuhl und tisch
-   {{3,90},{2,90},{0,90}},
-   {{0,90},{1,90}}
-  },
-  { // liege
-    {{0,90},{1,90}}
-  },
- { // Gäste
-    {{0,90},{1,90}}
- }
+int current_pos[3][3][2],
+  initial_pos[3][3][2]={
+   {{1,10},{2,90},{3,90}}, // S
+   {{4,90},{5,90},{6,170}}, // T
+   {{8,90},{9,100}}, // STB
 };
 
 int current_state = 0;
@@ -36,8 +34,7 @@ bool isRunning = false;
 bool isAllFurnitureSet = false;
 float difference;
 int movementDirection,
-  maxAmountLvls = 3,
-  maxAmountOfPieces = 2;
+  maxAmountLvls = 3;
 
 bool devmode = true;
 int how_many_seconds_to_build = 2 * 1000;
@@ -46,7 +43,9 @@ void setup() {
 //  current_pos = initial_pos;
   memcpy(current_pos,initial_pos,sizeof(initial_pos));
 
-  setPositions("2");
+  setPositions("0");
+  Serial.println("done");
+  resetPositions();
 }
 
 void loop() {
@@ -64,18 +63,18 @@ void setPositions(String situationID_string){
   {
     do { // until all furniture is set in this level
       isAllFurnitureSet = true; // nur ne Behauptung zum widerlegen
-      for(int piece=0;piece<maxAmountOfPieces;piece++) // each furniture
+      for(int piece=0;piece<3;piece++) // each furniture
       {
         if(target_pos[situationID][piece][0][0] != 0)
         {
-          difference = target_pos[situationID][piece][currentLvl][1] - current_pos[situationID][piece][currentLvl][1];
+          difference = target_pos[situationID][piece][currentLvl][1] - current_pos[piece][currentLvl][1];
           if(difference != 0)
           {
             isAllFurnitureSet = false;
             movementDirection = difference/abs(difference);
             // move one step closer to target
-            current_pos[situationID][piece][currentLvl][1] += movementDirection;
-            Serial.println(String(current_pos[situationID][piece][currentLvl][0]) + " => " + current_pos[situationID][piece][currentLvl][1] + "(" + difference + ")");
+            current_pos[piece][currentLvl][1] += movementDirection;
+              Serial.println(String(current_pos[piece][currentLvl][0]) + " => " + current_pos[piece][currentLvl][1] + "(" + difference + ")");
           }
         }
       }
@@ -84,6 +83,31 @@ void setPositions(String situationID_string){
   }
 
 	isRunning = false;
+}
+
+void resetPositions() {
+  isRunning = true; 
+  for(int currentLvl = maxAmountLvls-1;currentLvl >= 0;currentLvl--) // each level
+  {
+    do { // until all furniture is set in this level
+      isAllFurnitureSet = true; // nur ne Behauptung zum widerlegen
+      for(int piece=0;piece<3;piece++) // for each furniture
+      {
+          difference = initial_pos[piece][currentLvl][1] - current_pos[piece][currentLvl][1];
+          if(difference != 0)
+          {
+            isAllFurnitureSet = false;
+            movementDirection = difference/abs(difference);
+            // move one step closer to target
+            current_pos[piece][currentLvl][1] += movementDirection;
+            Serial.println(String(current_pos[piece][currentLvl][0]) + " => " + current_pos[piece][currentLvl][1] + "(" + difference + ")");
+          }
+      }
+      delay(5);
+    } while(isAllFurnitureSet == false);
+  }
+
+  isRunning = false;
 }
 
 
